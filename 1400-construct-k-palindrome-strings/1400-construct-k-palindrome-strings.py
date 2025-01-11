80d4b5e48b849665f0b8d0d8b4ea627403c11d7a
Ameer Hamza Khetran <hamzaa803@gmail.com>
2025-01-11 18:59:09 +0500

    Time: 61 ms (12.33%), Space: 18.3 MB (8.81%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -5,16 +5,11 @@ class Solution:
             return False
         if len(s) == k:
             return True
-        # Initialize frequency dictionary and odd_count
-        freq = [0] * 26
+        # Initialize oddCount as an integer bitmask
         odd_count = 0
 
-        # Increment the value of the index corresponding to the current character
-        for char in s:
-            freq[ord(char) - ord("a")] += 1
-        # Count the number of characters that appear an odd number of times in s
-        for count in freq:
-            if count % 2 == 1:
-                odd_count += 1
-        # Return if the number of odd frequencies is less than or equal to k
-        return odd_count <= k
+        # Update the bitmask for each character in the string
+        for chr in s:
+            odd_count ^= 1 << (ord(chr) - ord("a"))
+        # Return if the number of odd frequencies is less than or equal to
+        return bin(odd_count).count("1") <= k
