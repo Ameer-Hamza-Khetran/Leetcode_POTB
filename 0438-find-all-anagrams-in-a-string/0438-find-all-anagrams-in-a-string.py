e1dae28585ec755401b5881f2a6df108de37afa9
Ameer Hamza Khetran <hamzaa803@gmail.com>
2025-04-18 06:10:34 +0500

    Time: 35 ms (82.71%), Space: 18.5 MB (36.43%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,20 +1,21 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
-        len_s, len_p = len(s), len(p)
-
+        len_s = len(s)
+        len_p = len(p)
+        
         if len_p > len_s:
             return res
-
-        # Step 1: Build frequency map for p
+        
+        # Build frequency map for p
         p_map = {}
         for char in p:
             if char in p_map:
                 p_map[char] += 1
             else:
                 p_map[char] = 1
-
-        # Step 2: Initialize window map
+        
+        # initialize window map
         s_map = {}
         for i in range(len_p):
             char = s[i]
@@ -22,29 +23,29 @@ class Solution:
                 s_map[char] += 1
             else:
                 s_map[char] = 1
-
-        # Step 3: Compare first window
+        
+        # compare first window
         if s_map == p_map:
             res.append(0)
 
-        # Step 4: Slide the window
+        # slide the window
         for i in range(len_p, len_s):
             left_char = s[i - len_p]
             new_char = s[i]
 
-            # Remove left_char from window
+            # remove left char from the window
             s_map[left_char] -= 1
             if s_map[left_char] == 0:
                 del s_map[left_char]
 
-            # Add new_char to window
+            # add new_char to window
             if new_char in s_map:
                 s_map[new_char] += 1
             else:
                 s_map[new_char] = 1
 
-            # Compare maps
+            # compare maps
             if s_map == p_map:
-                res.append(i - len_p + 1)
-
-        return res
+                res.append(i-len_p + 1)
+        
+        return res 
