fe67542989db8de4005fc83b116e64a4febde3fe
Ameer Hamza Khetran <hamzaa803@gmail.com>
2025-05-13 17:23:34 +0500

    Time: 38 ms (67.24%), Space: 17.9 MB (23.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {}
+        
+        def helper(k):
+            if k in memo:
+                return memo[k]
+            if k == 0:
+                return 0
+            if k == 1:
+                return 1
+            memo[k] = helper(k-1) + helper(k-2)
+            return memo[k]
+        return helper(n)
