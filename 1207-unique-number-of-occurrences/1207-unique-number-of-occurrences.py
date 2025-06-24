6cf467ab5ddfa08a00a7325059a418c778d4f0a8
Ameer Hamza Khetran <hamzaa803@gmail.com>
2025-06-24 09:59:24 +0500

    Time: 0 ms (100%), Space: 18.1 MB (20.39%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,15 +1,15 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        count = [0] * 2001
+        freq_map = {}
         for num in arr:
-            count[num + 100] += 1
+            if num in freq_map:
+                freq_map[num] += 1
+            else:
+                freq_map[num] = 1
         
-        freq_seen = set()
-
-        for freq in count:
-            if freq == 0:
-                continue
-            if freq in freq_seen:
+        seen = set()
+        for freq in freq_map.values():
+            if freq in seen:
                 return False
-            freq_seen.add(freq)
+            seen.add(freq)
         return True
