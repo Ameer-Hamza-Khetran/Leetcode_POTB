4708ea1440a16992cdf833966dfb33c2637f434a
Ameer Hamza Khetran <hamzaa803@gmail.com>
2025-07-03 10:45:37 +0500

    Time: 45 ms (24.23%), Space: 30.2 MB (98.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,17 +5,28 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr = []
-        while(head):
-            arr.append(head.val)
-            head = head.next
-        i = 0
-        j = len(arr) - 1
+        # Step 1: Find the midpoint using fast and slow pointers
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse the first half of the list
+        prev = None
+        while head != slow:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+    
+        # Step 3: Compare the reversed first half with the second half
+        if fast:  # If the list has an odd number of elements, fast will not be None
+            slow = slow.next  # Skip the middle element for odd-length lists
 
-        while i <= j:
-            if arr[i] == arr[j]:
-                i += 1
-                j -= 1
-            else:
+        while prev:  # prev points to the reversed first half
+            if prev.val != slow.val:
                 return False
+            prev = prev.next
+            slow = slow.next
+        
         return True
